82471a6c0009f57122f3142d139b2d5ffe49950c
Antônio César <antonioazevedo.dev@gmail.com>
2025-02-18 13:26:17 -0300

    hotfix: adding gitignore

--- run_prizma_to_brapa.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "C:\Users\antonio.azevedo\Documents\Private\conversor-prizma-to-brapa"
-"C:\Users\antonio.azevedo\Documents\Private\conversor-prizma-to-brapa\.venv\Scripts\python.exe" "C:\Users\antonio.azevedo\Documents\Private\conversor-prizma-to-brapa\prizma_to_brapa
-"
